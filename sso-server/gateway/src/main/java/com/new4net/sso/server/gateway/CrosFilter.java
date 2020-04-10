bfec3811231f581d062347ed901ebf7a62749fa0
Administrator <admin@example.com>
2020-04-10 11:31:09 +0800

    test

--- sso-server/gateway/src/main/java/com/new4net/sso/server/gateway/CrosFilter.java
+++ sso-server/gateway/src/main/java/com/new4net/sso/server/gateway/CrosFilter.java
@@ -24,35 +24,44 @@ public class CrosFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if(HttpMethod.OPTIONS.equals(request.getMethod())){
+            response.setStatus(200);
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "*");
+            response.setHeader("Access-Control-Allow-Headers", "*");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setHeader("Access-Control-Expose-Headers", "Authorization");
+            return;
+        }else{
+            response.setHeader("Access-Control-Allow-Methods", "*");
+            response.setHeader("Access-Control-Allow-Headers", "Authorization,Origin, X-Requested-With, Content-Type, Accept");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setHeader("Access-Control-Expose-Headers", "Authorization");
 
-        response.setHeader("Access-Control-Allow-Methods", "*");
-        response.setHeader("Access-Control-Allow-Headers", "Authorization,Origin, X-Requested-With, Content-Type, Accept");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Expose-Headers", "Authorization");
-
-        String origin = request.getHeader("Origin");
+            String origin=request.getHeader("Origin");
 
 
-        if (StringUtils.isEmpty(origin)) {
-            if (StringUtils.isEmpty(allowDomain)) {
-                response.setHeader("Access-Control-Allow-Origin", "*");
-            } else {
-                response.setHeader("Access-Control-Allow-Origin", allowDomain);
-            }
-        } else {
-            String s1 = origin.startsWith("http://") ? origin.substring(7).trim() : origin.startsWith("https://") ? origin.substring(8).trim() : origin;
-            if (StringUtils.isEmpty(allowDomain)) {
-                response.setHeader("Access-Control-Allow-Origin", "*");
-            } else {
-                String s2 = allowDomain.startsWith("http://") ? allowDomain.substring(7).trim() : allowDomain.startsWith("https://") ? allowDomain.substring(8).trim() : allowDomain;
-                if (s1.contains(s2)) {
-                    response.setHeader("Access-Control-Allow-Origin", origin);
-                } else {
+            if(StringUtils.isEmpty(origin)){
+                if(StringUtils.isEmpty(allowDomain)){
+                    response.setHeader("Access-Control-Allow-Origin", "*");
+                }else {
                     response.setHeader("Access-Control-Allow-Origin", allowDomain);
                 }
+            }else{
+                String s1 = origin.startsWith("http://")?origin.substring(7).trim():origin.startsWith("https://")?origin.substring(8).trim():origin;
+                if(StringUtils.isEmpty(allowDomain)){
+                    response.setHeader("Access-Control-Allow-Origin", "*");
+                }else {
+                    String s2 = allowDomain.startsWith("http://")?allowDomain.substring(7).trim():allowDomain.startsWith("https://")?allowDomain.substring(8).trim():allowDomain;
+                    if(s1.contains(s2)){
+                        response.setHeader("Access-Control-Allow-Origin", origin);
+                    }else {
+                        response.setHeader("Access-Control-Allow-Origin", allowDomain);
+                    }
+                }
             }
-        }
 
+        }
 
         filterChain.doFilter(request, response);
 
@@ -65,7 +74,7 @@ public class CrosFilter extends OncePerRequestFilter {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        Thread thread = new Thread(() -> {
+        Thread thread = new Thread(()->{
             System.out.println("ok");
         });
         thread.join();
