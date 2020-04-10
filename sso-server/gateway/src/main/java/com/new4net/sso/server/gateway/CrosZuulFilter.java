f1718e2cd2bc22287dd63c415d706e81e05784c0
Administrator <admin@example.com>
2020-04-10 12:01:08 +0800

    test

---
+++ sso-server/gateway/src/main/java/com/new4net/sso/server/gateway/CrosZuulFilter.java
@@ -0,0 +1,60 @@
+package com.new4net.sso.server.gateway;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CrosZuulFilter  extends ZuulFilter {
+    @Value("${cros.allowDomain}")
+
+    private String allowDomain;
+    @Override
+    public String filterType() {
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        HttpServletResponse response = ctx.getResponse();
+        if(HttpMethod.OPTIONS.equals(request.getMethod())){
+            response.setStatus(200);
+            System.out.println("近来了");
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "*");
+            response.setHeader("Access-Control-Allow-Headers", "*");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setHeader("Access-Control-Expose-Headers", "Authorization");
+            try {
+                response.getWriter().write("");
+                response.getWriter().close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+        return null;
+    }
+}
